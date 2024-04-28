2e1487a96880b91cd1395926c9764374658d512d
Brandon N <brandonnelson2@gmail.com>
2024-04-28 22:14:34 +0100

    added javascript fuction for score tracting sectionand added fot and css styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,28 @@ const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 const choiceImage = ["✊", "🖐️", "✌️", "🦎", "🖖"];
 
 //score tracking
-let userScore = 0;
-let opponentScore = 0;
+let wins = 0;
+let losses = 0;
+let draws = 0;
+
+//score Updating functions
+function updateWin() {
+  let userScore = document.getElementById("wins");
+  wins++;
+  userScore.textContent = wins;
+}
+
+function updatelosses() {
+  let userLosses = document.getElementById("losses");
+  losses++;
+  userLosses.textContent = losses;
+}
+
+function updatedraws() {
+  let userDraws = document.getElementById("draws");
+  draws++;
+  userDraws.textContent = draws;
+}
 
 // function for generating a random integer number (not connected yet to choices)
 const randomInt = (max) => {
@@ -35,7 +55,10 @@ let playGame = (userChoice) => {
     console.log("unrecognised choice!");
     return; //Exits function early if userChoice isn't recognised
   }
+
   /*
+  Game rules:
+  
   rock beats = scissors and lizard
   paper beats = rock and spock
   scissors beats = lizard and paper
@@ -48,59 +71,74 @@ let playGame = (userChoice) => {
       if (opponentChoice === "scissors" || opponentChoice === "lizard") {
         updateImage(userChoice, opponentChoice);
         console.log("You win!");
+        updateWin();
       } else if (userChoice === opponentChoice) {
         updateImage(userChoice, opponentChoice);
+        updatedraws();
         console.log("Draw!");
       } else {
         updateImage(userChoice, opponentChoice);
+        updatelosses();
         console.log("You lose!");
       }
       break;
     case "paper":
       if (opponentChoice === "rock" || opponentChoice === "spock") {
         updateImage(userChoice, opponentChoice);
+        updateWin();
         console.log("You win!");
       } else if (userChoice === opponentChoice) {
         updateImage(userChoice, opponentChoice);
+        updatedraws();
         console.log("Draw!");
       } else {
         updateImage(userChoice, opponentChoice);
+        updatelosses();
         console.log("You lose!");
       }
       break;
     case "scissors":
       if (opponentChoice === "lizard" || opponentChoice === "paper") {
         updateImage(userChoice, opponentChoice);
+        updateWin();
         console.log("You win!");
       } else if (userChoice === opponentChoice) {
         updateImage(userChoice, opponentChoice);
+        updatedraws();
         console.log("Draw!");
       } else {
         updateImage(userChoice, opponentChoice);
+        updatelosses();
         console.log("You lose!");
       }
       break;
     case "lizard":
       if (opponentChoice === "paper" || opponentChoice === "spock") {
         updateImage(userChoice, opponentChoice);
+        updateWin();
         console.log("You win!");
       } else if (userChoice === opponentChoice) {
         updateImage(userChoice, opponentChoice);
+        updatedraws();
         console.log("Draw!");
       } else {
         updateImage(userChoice, opponentChoice);
+        updatelosses();
         console.log("You lose!");
       }
       break;
     case "spock":
       if (opponentChoice === "rock" || opponentChoice === "scissors") {
         console.log(`Oponant: ${opponentChoice}`);
+        updateWin();
         console.log("You win!");
       } else if (userChoice === opponentChoice) {
         updateImage(userChoice, opponentChoice);
+        updatedraws();
         console.log("Draw!");
       } else {
         updateImage(userChoice, opponentChoice);
+        updatelosses();
         console.log("You lose!");
       }
       break;
